1cfb12a4e100b2c6c484d984f6be4fcc97380c5e
Arihant <joshi11.arihant@gmail.com>
2020-09-03 10:47:49 +0530

    Updated hi.py to Loop

--- hi.py
+++ hi.py
@@ -1,7 +1,15 @@
 import sys
 
 def main():
-	print("Hello~",str(sys.argv[1]))
+	name = "Jenkins User"
+	count = 1
+	if(len(sys.argv) == 3):
+		count = sys.argv[2]
+	if(len(sys.argv) == 2):
+		name = sys.argv[1]
+		
+	for i in range(count):
+		print("Hello~",str(name))
 
 if __name__ == "__main__":
 	main()
