2ef6b1b85d27020987291934785f2eef29b4db37
Arihant <joshi11.arihant@gmail.com>
2020-09-03 10:48:30 +0530

    Updated hi.py to Loop

--- hi.py
+++ hi.py
@@ -6,8 +6,8 @@ def main():
 	if(len(sys.argv) == 3):
 		count = sys.argv[2]
 	if(len(sys.argv) == 2):
-		name = sys.argv[1]
-		
+		name = int(sys.argv[1])
+
 	for i in range(count):
 		print("Hello~",str(name))
 
